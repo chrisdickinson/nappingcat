db4e86a240342cd3e6799ed84755573e07c08755
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-04-07 12:48:45 -0500

    attempt to print the results of whatever ssh command was handled.

--- nappingcat/app.py
+++ nappingcat/app.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from nappingcat.exceptions import NappingCatUnhandled
+from nappingcat.exceptions import NappingCatUnhandled, NappingCatException
 
 class App(object):
     def __init__(self, *args, **kwargs):
@@ -10,7 +10,11 @@ class App(object):
     def run(cls, *args, **kwargs):
         user = sys.argv[1]
         original_command = os.environ.get('SSH_ORIGINAL_COMMAND') 
-        return cls().main(user=user, original_command=original_command)
+        try:
+            results = cls().main(user=user, original_command=original_command)
+            print >>sys.stderr, "\033[0;32m%s\033[0m" % results
+        except NappingCatException, e:
+            print >>sys.stderr, "\033[1;31m%s\033[0m" % e 
 
     def main(self, *args, **kwargs):
         raise NappingCatUnhandled("""
