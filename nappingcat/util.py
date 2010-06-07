78828a7926a7f5b5aa51cba1893ed6e1ecae21fb
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-06-07 10:06:10 -0500

    move to standard importlib import_module

--- nappingcat/util.py
+++ nappingcat/util.py
@@ -1,6 +1,5 @@
-# ripped off of django, erk
-# Taken from Python 2.7 with permission from/by the original author.
 import sys
+from importlib import import_module
 
 def _resolve_name(name, package, level):
     """Return the absolute name of the module to be imported."""
@@ -14,28 +13,6 @@ def _resolve_name(name, package, level):
             raise ValueError("attempted relative import beyond top-level "
                               "package")
     return "%s.%s" % (package[:dot], name)
-
-
-def import_module(name, package=None):
-    """Import a module.
-
-    The 'package' argument is required when performing a relative import. It
-    specifies the package to use as the anchor point from which to resolve the
-    relative import to an absolute import.
-
-    """
-    if name.startswith('.'):
-        if not package:
-            raise TypeError("relative imports require the 'package' argument")
-        level = 0
-        for character in name:
-            if character != '.':
-                break
-            level += 1
-        name = _resolve_name(name[level:], package, level)
-    __import__(name)
-    return sys.modules[name]
-
 
 def import_class_from_module(name):
     module_path, target = name.rsplit('.', 1)
