7af4f11c89914d3b6123943d73c570233b64831f
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-06-07 17:09:02 -0500

    fix regexen for git handlers

--- nappingcat/contrib/git/patterns.py
+++ nappingcat/contrib/git/patterns.py
@@ -1,9 +1,9 @@
 from nappingcat.patterns import patterns
 
 cmdpatterns = patterns('nappingcat.contrib.git.handlers',
-    (r'^kitty-git fork \'(?P<repo>[\w\/]+).git\'', 'fork_repo'),
-    (r'^kitty-git create-repo \'(?P<repo_name>[\w\-\\\.]+)\'( --template=\'(?P<template_dir>.*)\')?', 'create_repo'),
-    (r'^kitty-git delete-repo \'(?P<repo_name>[\w\\\-\.]+)\'', 'delete_repo'),
+    (r'^kitty-git fork \'(?P<repo>[\w\.\-\\\/]+).git\'', 'fork_repo'),
+    (r'^kitty-git create-repo \'(?P<repo_name>[\w\/\\\-\.]+)\'( --template=\'(?P<template_dir>.*)\')?', 'create_repo'),
+    (r'^kitty-git delete-repo \'(?P<repo_name>[\w\/\\\-\.]+)\'', 'delete_repo'),
     (r'^kitty-git list (?P<username>[\w\\\-\.]+)', 'list_repos'),
     (r'^git(?P<action>[\-a-zA-Z_0-9\s]+)', 'handle_git'),
 )
