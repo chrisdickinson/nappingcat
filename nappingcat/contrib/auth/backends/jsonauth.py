557cadb5dbe4a198bf931d4d2ccc96a0065c4356
Chris Dickinson <christopher.s.dickinson@gmail.com>
2010-06-12 13:37:17 -0500

    make json auth backend actually pay attention to nappingcat.conf location of auth json file

--- nappingcat/contrib/auth/backends/jsonauth.py
+++ nappingcat/contrib/auth/backends/jsonauth.py
@@ -1,16 +1,17 @@
 from nappingcat.auth import AuthBackend
-from nappingcat import config
 import os
 try:
     import json as simplejson
 except ImportError:
     import simplejson
 
+
+SECTION_NAME = 'jsonauth'
 class JSONAuthBackend(AuthBackend):
     def __init__(self, *args, **kwargs):
         super(JSONAuthBackend, self).__init__(*args, **kwargs)
-        settings_dict = dict(self.settings.items(config.SECTION_NAME))
-        filename = os.path.expanduser(settings_dict.get('jsonauth', '~/nappingcat_auth.json'))
+        settings_dict = dict(self.settings.items(SECTION_NAME))
+        filename = os.path.expanduser(settings_dict.get('file', '~/nappingcat_auth.json'))
         try:
             with open(filename, 'r') as input:
                 self.users = simplejson.loads(input.read())
@@ -22,7 +23,7 @@ class JSONAuthBackend(AuthBackend):
     def finish(self, pubkey_handler):
         super(JSONAuthBackend, self).finish(pubkey_handler)
         if self.require_update:
-            settings_dict = dict(self.settings.items(config.SECTION_NAME))
-            filename = os.path.expanduser(settings_dict.get('jsonauth', '~/nappingcat_auth.json'))
+            settings_dict = dict(self.settings.items(SECTION_NAME))
+            filename = os.path.expanduser(settings_dict.get('file', '~/nappingcat_auth.json'))
             with open(filename, 'w') as output:
                 output.write(simplejson.dumps(self.users))
